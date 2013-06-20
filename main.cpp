e2da117dac981edf15ed52360e58d374455c4ac6
Ovaphlow <jsfreecss@163.com>
2013-06-20 14:33:38 +0800

    Signed-off-by: Ovaphlow <jsfreecss@163.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QLabel>
+
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
+  QApplication app(argc, argv);
+  QWidget *window = new QWidget;
+  window->setWindowTitle("输入年龄");
+  QSpinBox *spinBox = new QSpinBox;
+  QSlider *slider = new QSlider(Qt::Horizontal);
+  spinBox->setRange(0, 130);
+  slider->setRange(0, 130);
 
-  return a.exec();
+  QObject::connect(spinBox, SIGNAL(valueChanged(int)),
+                   slider, SLOT(setValue(int)));
+  QObject::connect(slider, SIGNAL(valueChanged(int)),
+                   spinBox, SLOT(setValue(int)));
+  spinBox->setValue(35);
+
+  QHBoxLayout *topLayout = new QHBoxLayout;
+  topLayout->addWidget(spinBox);
+  topLayout->addWidget(slider);
+
+ // QLabel *labelImage = new QLabel;
+
+  window->setLayout(topLayout);
+
+  window->show();
+
+  return app.exec();
 }
